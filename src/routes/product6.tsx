d0f198863151132f20cd742aa93b1dee16de8a6d
aoi20031020 <aoi20031020@gmail.com>
2024-02-12 02:24:16 +0900

    猫ミーム

--- src/routes/product6.tsx
+++ src/routes/product6.tsx
@@ -1,4 +1,20 @@
 import { Text } from "@chakra-ui/react";
+import video_mp4 from "../videos/頭を抱える猫.mp4";
+import React, { useEffect, useRef } from "react";
+
 export const Product6 = () => {
-  return <Text fontSize="5xl">product6</Text>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    videoRef.current?.play();
+  }, []);
+  return (
+    <>
+      <Text fontSize="5xl">product6</Text>
+      <React.StrictMode>
+        <video controls muted ref={videoRef}>
+          <source src={video_mp4} type="video/mp4" />
+        </video>
+      </React.StrictMode>
+    </>
+  );
 };
--- src/routes/product7.tsx
+++ src/routes/product7.tsx
@@ -1,4 +1,20 @@
 import { Text } from "@chakra-ui/react";
+import video_mp4 from "../videos/米津ハッピー.mp4";
+import { useEffect, useRef } from "react";
+import React from "react";
 export const Product7 = () => {
-  return <Text fontSize="5xl">product7</Text>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    videoRef.current?.play();
+  }, []);
+  return (
+    <>
+      <Text fontSize="5xl">product6</Text>
+      <React.StrictMode>
+        <video controls muted ref={videoRef}>
+          <source src={video_mp4} type="video/mp4" />
+        </video>
+      </React.StrictMode>
+    </>
+  );
 };
